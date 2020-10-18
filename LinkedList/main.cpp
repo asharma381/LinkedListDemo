e2421e852d4074e4c638b33cd52aac9422ee7423
Aditya Sharma <adityasharma1709@gmail.com>
2020-10-17 23:10:03 -0700

    Initial Commit

---
+++ LinkedList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinkedList
+//
+//  Created by Aditya Sharma on 10/17/20.
+//  Copyright © 2020 Aditya Sharma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
